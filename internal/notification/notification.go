58f25cf8873eb03f8967afc5fafac777de03fe27
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2023-05-30 17:35:27 +0200

    refactor: add dummy notification interface

--- internal/notification/notification.go
+++ internal/notification/notification.go
@@ -8,3 +8,13 @@ type Notification interface {
 	NotifyUpdatedIpDetected(ip *common.ResolvedIp) error
 	NotifyUpdatedIpApplied(ip *common.ResolvedIp) error
 }
+
+type DummyNotification struct{}
+
+func (d *DummyNotification) NotifyUpdatedIpDetected(ip *common.ResolvedIp) error {
+	return nil
+}
+
+func (d *DummyNotification) NotifyUpdatedIpApplied(ip *common.ResolvedIp) error {
+	return nil
+}
