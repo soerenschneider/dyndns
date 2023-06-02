7ca9cd50dfc6d34eebe81e67c33249c55072326a
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2023-06-02 08:18:52 +0200

    fix: define custom validator to fix panic

--- conf/validation.go
+++ conf/validation.go
@@ -3,6 +3,7 @@ package conf
 import (
 	"github.com/go-playground/validator/v10"
 	"net/url"
+	"reflect"
 	"sync"
 )
 
@@ -14,9 +15,34 @@ var (
 func ValidateConfig[T any](c T) error {
 	once.Do(func() {
 		validate = validator.New()
+		validate.RegisterValidation("addrfamilies", validateAddrFamilies)
 	})
 
 	return validate.Struct(c)
+}
+
+func validateAddrFamilies(fl validator.FieldLevel) bool {
+	// Get the field value and check if it's a slice
+	field := fl.Field()
+	if field.Kind() != reflect.Slice {
+		return false
+	}
+
+	for i := 0; i < field.Len(); i++ {
+		item := field.Index(i)
+
+		// Convert to string and check its value
+		str, ok := item.Interface().(string)
+		if !ok {
+			return false
+		}
+
+		if str != AddrFamilyIpv4 && str != AddrFamilyIpv6 {
+			return false
+		}
+	}
+
+	return true
 }
 
 func IsValidUrl(input string) bool {
