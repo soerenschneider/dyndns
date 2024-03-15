51012d3f3926382d7fe45fc79dcd06b63445a34e
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-03-15 21:08:54 +0100

    fix: fix config validation

--- conf/http.go
+++ conf/http.go
@@ -1,7 +1,7 @@
 package conf
 
 type HttpConfig struct {
-	ListenAddr string `yaml:"addr" validate:""`
-	TlsCert    string `yaml:"tls_cert" validate:"required_with=TlsKey,file"`
-	TlsKey     string `yaml:"tls_key" validate:"required_with=TlsCert,file"`
+	ListenAddr string `yaml:"addr"`
+	TlsCert    string `yaml:"tls_cert" validate:"required_with_all=TlsKey,omitempty,file"`
+	TlsKey     string `yaml:"tls_key" validate:"required_with_all=TlsCert,omitempty,file"`
 }
