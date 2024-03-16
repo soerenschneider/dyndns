13271063a21f297306d6398e1b380284a360a933
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2024-03-16 12:08:55 +0100

    refactor: delete message after reading it

--- internal/events/sqs/sqs_dispatch.go
+++ internal/events/sqs/sqs_dispatch.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"sync"
 	"time"
 
@@ -19,10 +18,14 @@ import (
 	"go.uber.org/multierr"
 )
 
+const defaultWaitTimeSeconds = 20
+
 type SqsListener struct {
 	client   *sqs.SQS
 	queueUrl string
 	requests chan common.UpdateRecordRequest
+
+	waitTimeSeconds int64
 }
 
 type SqsOpts func(consumer *SqsListener) error
@@ -33,8 +36,9 @@ func NewSqsConsumer(sqsConf conf.SqsConfig, provider credentials.Provider, reqCh
 	}
 
 	ret := &SqsListener{
-		queueUrl: sqsConf.SqsQueue,
-		requests: reqChan,
+		queueUrl:        sqsConf.SqsQueue,
+		requests:        reqChan,
+		waitTimeSeconds: defaultWaitTimeSeconds,
 	}
 
 	var errs error
@@ -85,27 +89,42 @@ func (h *SqsListener) Listen(ctx context.Context, wg *sync.WaitGroup) error {
 
 func (h *SqsListener) fetchMessages(ctx context.Context) error {
 	log.Debug().Msg("Trying to receive SQS messages")
+	metrics.SqsApiCalls.WithLabelValues("receive_message").Inc()
 	result, err := h.client.ReceiveMessageWithContext(ctx, &sqs.ReceiveMessageInput{
 		QueueUrl:            aws.String(h.queueUrl),
 		MaxNumberOfMessages: aws.Int64(10),
 		VisibilityTimeout:   aws.Int64(30),
-		WaitTimeSeconds:     aws.Int64(20),
+		WaitTimeSeconds:     aws.Int64(h.waitTimeSeconds),
 	})
-
 	if err != nil {
 		return err
 	}
 
+	var errs error
 	for _, message := range result.Messages {
 		if err := h.handleMessage(message); err != nil {
-			log.Error().Err(err).Msgf("handling received message failed: %v", err)
+			errs = multierr.Append(errs, err)
 		}
 	}
 
-	return nil
+	return errs
 }
 
 func (h *SqsListener) handleMessage(message *sqs.Message) error {
+	defer func() {
+		// the client is not going to stop ip update requests as long the ip has not been updated, so we have the luxury
+		// to not care about edge cases too much and delete the message after receiving it.
+		log.Debug().Msgf("Deleting message %q from queue", *message.MessageId)
+		_, err := h.client.DeleteMessage(&sqs.DeleteMessageInput{
+			QueueUrl:      aws.String(h.queueUrl),
+			ReceiptHandle: message.ReceiptHandle,
+		})
+		if err != nil {
+			log.Error().Err(err).Msgf("Could not delete message %q from queue", *message.MessageId)
+		}
+		metrics.SqsApiCalls.WithLabelValues("delete_message").Inc()
+	}()
+
 	if message == nil || message.Body == nil {
 		log.Warn().Msg("Received empty SQS message")
 		return nil
@@ -114,14 +133,6 @@ func (h *SqsListener) handleMessage(message *sqs.Message) error {
 	err := h.dispatch([]byte(*message.Body))
 	if err != nil {
 		return err
-	}
-
-	_, err = h.client.DeleteMessage(&sqs.DeleteMessageInput{
-		QueueUrl:      aws.String(h.queueUrl),
-		ReceiptHandle: message.ReceiptHandle,
-	})
-	if err != nil {
-		return fmt.Errorf("could not delete message %q from queue: %w", *message.MessageId, err)
 	}
 
 	return nil
