c2c3d0a75cb6ac387916500bb328995f5193db24
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-06-10 20:03:45 +0200

    refactor: don't ignore error

--- internal/client/resolvers/http_resolver.go
+++ internal/client/resolvers/http_resolver.go
@@ -177,7 +177,9 @@ func resolveSingle(url string, client *http.Client) (string, error) {
 	timeTaken := time.Since(start)
 	metrics.ResponseTime.WithLabelValues(url).Observe(timeTaken.Seconds())
 
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", fmt.Errorf("couldn't read response: %v", err)
